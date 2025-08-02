9bd13dfa4a21a5bc2b037e85a5933576ad0a5ed2
Iryna-M-html <ivmohnataya@gmail.com>
2025-08-02 17:49:11 +0300

    Fix to wishlist

--- src/wishlist.js
+++ src/wishlist.js
@@ -3,7 +3,7 @@ import {updateWishlistCounter }  from './js/helpers';
 import { refs } from './js/refs';
 import { isInWishlist, getWishlist } from './js/storage';
 import { fetchProductsByIds } from './js/products-api';
-import { renderProducts } from './js/render-function';
+import { renderProducts, renderEmptyMessage } from './js/render-function';
 import { handleProductsListItemClick, addProductByIdToWishlist, addProductByIdToCart } from './js/handlers';
 import { closeModal } from './js/modal';
 
@@ -12,18 +12,22 @@ const LoadWishListProducts = async () => {
     const wishlistItems = getWishlist();
 
     if (wishlistItems.length === 0) {
-    //renderEmptyMessage(refs.productsList, 'Your wishlist is empty.');
-    return; 
+        renderEmptyMessage(refs.productsList, 'Your wishlist is empty.');
+        return; 
     } 
 
     const wishlistProducts = await fetchProductsByIds(wishlistItems);
     renderProducts(wishlistProducts);
 
 }
-
+const closeModalWishlist = () => {
+    closeModal();
+    LoadWishListProducts();
+}
+ 
 LoadWishListProducts();
 refs.productsList.addEventListener("click", handleProductsListItemClick);
-refs.modalCloseBtn.addEventListener("click", closeModal);
+refs.modalCloseBtn.addEventListener("click", closeModalWishlist);
 refs.addToCartBtn.addEventListener('click', addProductByIdToCart);
 refs.addToWishlistBtn.addEventListener('click', addProductByIdToWishlist);
 updateWishlistCounter();
