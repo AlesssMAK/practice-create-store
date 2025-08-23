fc3e1b35572472c1171e05341fa32e2e00432c80
Iryna-M-html <ivmohnataya@gmail.com>
2025-08-23 12:52:22 +0300

    DarkLight Theme

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,35 +1,48 @@
 //Логіка сторінки Wishlist
-import {updateWishlistCounter, updateCartCounter}  from './js/helpers';
+import { updateWishlistCounter, updateCartCounter } from './js/helpers';
 import { refs } from './js/refs';
-import { isInWishlist, getWishlist } from './js/storage';
+import {
+  isInWishlist,
+  getWishlist,
+  getTheme,
+  applyTheme,
+  toggleTheme,
+} from './js/storage';
 import { fetchProductsByIds } from './js/products-api';
 import { renderProducts, renderEmptyMessage } from './js/render-function';
-import { handleProductsListItemClick, addProductByIdToWishlist, addProductByIdToCart } from './js/handlers';
+import {
+  handleProductsListItemClick,
+  addProductByIdToWishlist,
+  addProductByIdToCart,
+} from './js/handlers';
 import { closeModal } from './js/modal';
 
-
+applyTheme(getTheme());
+const themeToggleBtn = document.querySelector('.theme-toggle-btn');
+if (themeToggleBtn) {
+  themeToggleBtn.addEventListener('click', toggleTheme);
+}
 
 const LoadWishListProducts = async () => {
-    const wishlistItems = getWishlist();
-    
-    if (wishlistItems.length === 0) {
-        renderEmptyMessage(refs.productsList, 'Your wishlist is empty.');
-        return; 
-    } 
+  const wishlistItems = getWishlist();
 
-    const wishlistProducts = await fetchProductsByIds(wishlistItems);
-    refs.productsList.innerHTML = '';
-    renderProducts(wishlistProducts);
+  if (wishlistItems.length === 0) {
+    renderEmptyMessage(refs.productsList, 'Your wishlist is empty.');
+    return;
+  }
 
-}
+  const wishlistProducts = await fetchProductsByIds(wishlistItems);
+  refs.productsList.innerHTML = '';
+  renderProducts(wishlistProducts);
+};
 const closeModalWishlist = () => {
-    closeModal();
-    LoadWishListProducts();
-}
- 
+  closeModal();
+  LoadWishListProducts();
+};
+
 LoadWishListProducts();
-refs.productsList.addEventListener("click", handleProductsListItemClick);
-refs.modalCloseBtn.addEventListener("click", closeModalWishlist);
+refs.productsList.addEventListener('click', handleProductsListItemClick);
+refs.modalCloseBtn.addEventListener('click', closeModalWishlist);
 refs.addToCartBtn.addEventListener('click', addProductByIdToCart);
 refs.addToWishlistBtn.addEventListener('click', addProductByIdToWishlist);
 updateWishlistCounter();
